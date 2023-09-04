60cc967038544f8ace6922d8f2de6172cbb72f6f
성민 <63378895+dureotkd@users.noreply.github.com>
2023-09-04 14:44:05 +0900

    github actions 추가

--- server/model/core/database.js
+++ server/model/core/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const db = mysql.createPoolCluster();
 
 db.add("code_exam", {
-  host: "52.79.106.68",
+  host: "43.200.131.181",
   user: "root",
   password: "@Slsksh671201@",
   database: "code_exam",
