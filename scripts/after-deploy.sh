cb4436d9347f2428351bc912a11b73f13762c8fa
성민 <63378895+dureotkd@users.noreply.github.com>
2023-09-05 13:19:24 +0900

    hello

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+REPOSITORY=/home/ec2-user/project/front-end-exam
 SERVER_REPOSITORY=/home/ec2-user/project/front-end-exam/server
+CLIENT_REPOSITORY=/home/ec2-user/project/front-end-exam/client
 
 source ~/.bashrc
+
+cd $REPOSITORY
+
+git pull origin master
+
 
 cd $SERVER_REPOSITORY
 
 npx yarn
 npx pm2 reload server.js
+
+
+cd $CLIENT_REPOSITORY
+
+npx yarn
+npx npm run build
